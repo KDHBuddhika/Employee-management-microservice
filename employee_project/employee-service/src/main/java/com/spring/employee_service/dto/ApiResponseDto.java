d575013e0f387f26680f705060df2c0ccc188c31
KDHBuddhika <136336705+KDHBuddhika@users.noreply.github.com>
2025-07-03 17:38:22 +0530

    set communication between services

--- employee_project/employee-service/src/main/java/com/spring/employee_service/dto/ApiResponseDto.java
+++ employee_project/employee-service/src/main/java/com/spring/employee_service/dto/ApiResponseDto.java
@@ -1,18 +1,16 @@
 package com.spring.employee_service.dto;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class EmployeeDto {
-    private Long id;
-    private String firstName;
-    private String lastName;
-    private String email;
-    private  String departmentCode;
+@Getter
+@Setter
+public class ApiResponseDto {
+    private EmployeeDto employeeDto;
+    private DepartmentDto departmentDto;
 }
