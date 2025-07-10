6913b6ec85859e540b43fe70dd3c82d2595a4c2c
KDHBuddhika <136336705+KDHBuddhika@users.noreply.github.com>
2025-07-10 23:09:43 +0530

    register two services

--- employee_project/employee-service/src/main/java/com/spring/employee_service/EmployeeServiceApplication.java
+++ employee_project/employee-service/src/main/java/com/spring/employee_service/EmployeeServiceApplication.java
@@ -2,6 +2,7 @@ package com.spring.employee_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -9,6 +10,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableFeignClients
+
 public class EmployeeServiceApplication {
 
 	public static void main(String[] args) {
